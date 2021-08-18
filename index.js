317bc9550833cd8329844119900af75a6bda248b
Philip Wofford <professionalphilip@gmail.com>
2021-08-18 14:06:33 -0400

    added a placeholder for an /about route

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ let app = express();
 
 const PORT = 8080;
 
+app.get('/about', function (req, res, next) {
+	res.send(`Emily is so great!`);
+})
+
 app.get('*', function (req, res, next) {
   res.send(`Under Construction by Emdog's Em-Dogs!`);
 })
